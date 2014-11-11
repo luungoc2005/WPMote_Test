b1fb9409fc89bb25ee42c7242ce20e330f7aa6cc
luungoc2005 <luungoc2005@gmail.com>
2014-11-11 10:43:39 +0700

    ClientInfo message
    first message struct

--- WPMote_Desk/WPMote_Desk/Connectivity/Messages/Comm_Message.cs
+++ WPMote_Desk/WPMote_Desk/Connectivity/Messages/Comm_Message.cs
@@ -20,8 +20,99 @@ namespace WPMote.Connectivity.Messages
 
         internal static Dictionary<byte, UInt16> dictMessages = new Dictionary<byte, UInt16> 
         { 
-            {100,sizeof(UInt16)}
+            {100,sizeof(Int16)}, //TEST CMD
+            {101,4*sizeof(byte)+sizeof(Int16)+128} //ClientInfo: IP & HostName, MachineName 128 chars max
         };
+
+        #endregion
+        
+        #region "Events"
+
+
+
+        #endregion
+
+        #region "Message Structs"
+
+        struct Msg_ClientInfo
+        {
+            byte ID = 101;
+            string IPAddress;
+            string MachineName;
+
+            //Constructors
+            void Msg_ClientInfo(byte[] bData)
+            {
+                var objStream = new MemoryStream(bData);
+                var objRead = new BinaryReader(objStream);
+
+                try
+                {
+                    IPAddress = objRead.ReadByte().ToString() + "." +
+                        objRead.ReadByte().ToString() + "." +
+                        objRead.ReadByte().ToString() + "." +
+                        objRead.ReadByte().ToString();
+
+                    Int16 strLength = objRead.ReadInt16();
+                    string strData = Encoding.Unicode.GetString(objRead.ReadBytes(128), 0, strLength);
+
+                    MachineName = strData;
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    objRead.Dispose();
+                    objStream.Dispose();
+                }
+            }
+
+            void Msg_ClientInfo(string strIP, string strName)
+            {
+                IPAddress = strIP;
+                MachineName = strName;
+            }
+
+            //To byte array
+            readonly byte[] ToByteArray
+            {
+                get
+                {
+                    var bData = new byte[dictMessages[ID]];
+                    var objStream = new MemoryStream(bData);
+                    var objWrite = new BinaryWriter(objStream);
+
+                    try
+                    {
+                        //IP Address to byte()
+                        string[] strIPTemp = IPAddress.Split('.');
+
+                        if (strIPTemp.Length != 4) throw new Exception("Invalid IP Address");
+                        foreach (var temp in strIPTemp)
+                        {
+                            objWrite.Write(Convert.ToByte(temp));
+                        }
+
+                        objWrite.Write((Int16)Math.Min(MachineName.Length, 128));
+                        objWrite.Write(Encoding.Unicode.GetBytes(MachineName));
+                        objWrite.Flush();
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        objWrite.Dispose();
+                        objStream.Dispose();
+                    }
+
+                    return bData;
+                }
+            }
+        }
 
         #endregion
 
