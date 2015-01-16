2b9c179c9e049fe7a3d72e6161d8c7d80bac5b6a
luungoc2005 <luungoc2005@gmail.com>
2015-01-16 10:57:23 +0700

    More work on separate mouse processor

--- WPMote_Desk/WPMote_Desk/Processor/MouseProcessor.cs
+++ WPMote_Desk/WPMote_Desk/Processor/MouseProcessor.cs
@@ -16,9 +16,68 @@ namespace WPMote_Desk.Processor
         private static Object _syncRoot = new Object();
 
         private List<Simple3DVector> readingsQueue = new List<Simple3DVector>();
+
+        private Simple3DVector currentVelocity;
+
+        private Simple3DVector previousReading = new Simple3DVector();
+
+        private Timer lagTimer = new Timer();
+
+        private int adjustmentInterval = 800;
+        private int stableSamplesCount = 0;
+        private Simple3DVector maximumStableOffset = new Simple3DVector(0.007, 0.007, 0.007);
+
+        private const int coordinateMulFactor = 9;
+        #endregion
+
+        #region "Class constructor"
+        public MouseProcessor()
+        {
+            //Lag timer initialization
+            lagTimer.Interval = 20;
+            lagTimer.Tick += lagTimer_Tick;
+        }
+        
+        #endregion
+        
+        #region "Private methods"
+        private void lagTimer_Tick(object sender, EventArgs e)
+        {
+            //Timer for lag compensation
+            ProcessNextReading();
+        }
+
+        private void ProcessNextReading()
+        {
+            if (readingsQueue.Count != 0)
+            {
+                currentVelocity += readingsQueue[0] / (1000 / lagTimer.Interval);
+
+                //return velocity to 0 after a certain stable period
+                if ((readingsQueue[0] - previousReading).Magnitude <= maximumStableOffset.Magnitude)
+                {
+                    stableSamplesCount += lagTimer.Interval;
+                    if (stableSamplesCount >= adjustmentInterval)
+                    {
+                        currentVelocity = new Simple3DVector();
+                    }
+                }
+                else
+                {
+                    stableSamplesCount = 0;
+                }
+                previousReading = readingsQueue[0];
+
+                readingsQueue.RemoveAt(0);
+
+                //move mouse pointer
+                Win32.MousePointer.Move(new Point((int)currentVelocity.X * coordinateMulFactor, (int)currentVelocity.Y * coordinateMulFactor));
+            }
+        }
         #endregion
 
         #region "Public methods"
+
         public static MouseProcessor Instance
         {
             get
@@ -36,6 +95,22 @@ namespace WPMote_Desk.Processor
                 return _singletonInstance;
             }
         }
+
+        public void AddReading(Simple3DVector sensorReading)
+        {
+            readingsQueue.Add(sensorReading);
+        }
+
+        public void Start()
+        {
+            lagTimer.Start();
+        }
+
+        public void Stop()
+        {
+            lagTimer.Stop();
+        }
+
         #endregion
 
         //0,0: -0.467 -0.6237 -0.6675
